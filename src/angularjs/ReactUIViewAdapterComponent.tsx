c5a7fa3461fbcd403929ce1a54c4e0da4e81764e
christopherthielen <github@sandgnat.com>
2018-03-10 13:46:43 -0800

    fix(react-hybrid): Fix errors surfaced by react 16

--- src/angularjs/ReactUIViewAdapterComponent.tsx
+++ src/angularjs/ReactUIViewAdapterComponent.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import * as angular from 'angular';
 import * as ReactDOM from 'react-dom';
 import { hybridModule } from './module';
 import { UIView, UIViewProps } from '@uirouter/react';
+import { filter } from '@uirouter/core';
 import { UIRouterContextComponent } from '../react/UIRouterReactContext';
 
 // When an angularjs `ui-view` is instantiated, also create an adapter (which creates a react UIView)
@@ -11,10 +13,13 @@ hybridModule.directive('uiView', function () {
     compile: function (tElem, tAttrs) {
       let { name, uiView } = tAttrs;
       name = name || uiView || '$default';
-      tElem.html(`<react-ui-view-adapter name="${name}">default content</react-ui-view-adapter>`);
+      // console.log('Creating react-ui-view-adapter', tElem);
+      tElem.html(`<react-ui-view-adapter name="${name}"></react-ui-view-adapter>`);
     }
   }
 });
+
+var id = 0;
 
 // This angularjs adapter (inside an angularjs ui-view) creates the react UIView and provides it the correct context
 // It also allows angularjs children created inside the react view (via angular2react or whatever) to access the correct
@@ -25,17 +30,26 @@ hybridModule.directive('reactUiViewAdapter', function () {
     link: function (scope, elem, attrs) {
       const el = elem[0];
       let _ref = null;
+      let destroyed = false;
+      const $id = id++;      
+      const ignoredAttrKeys = ['$$element', '$attr'];
+      attrs = filter(attrs, (val, key) => ignoredAttrKeys.indexOf(key) === -1) as any;
 
-      const log = (msg, ref) => {
-        const id = ref && ref.state && ref.state.id;
-        const cmp = ref && ref.componentInstance;
-        console.log(msg, !!ref, id, cmp);
+      // console.log(`${$id}: linking react-ui-view-adapter into `, el, attrs)
+      
+      const log = (msg, UIViewRef) => {
+        const id = UIViewRef && UIViewRef.state && UIViewRef.state.id;
+        const cmp = UIViewRef && UIViewRef.componentInstance;
+        console.log(msg, `Has UIViewRef: ${!!UIViewRef}`, id, cmp);
       };
 
       // The UIView ref callback, which is called after the initial render
       const ref = ref => {
+        if (ref && _ref === ref) return;
         _ref = ref;
-        // log('ref', _ref);
+
+        // log(`${$id}: received new React UIView ref:`, ref);
+
         // Add the $uiView data to the adapter element to provide context to child angular elements
         provideContextToAngularJSChildren();
         renderReactUIView();
@@ -60,13 +74,21 @@ hybridModule.directive('reactUiViewAdapter', function () {
       };
 
       function renderReactUIView() {
-        const uiView = React.createElement(UIView, { ...attrs, render, ref, wrap: false } as UIViewProps);
-        const reactEl: React.ReactElement<any> = React.createElement(UIRouterContextComponent, { parentContextLevel: '3' }, uiView);
-        ReactDOM.render<any>(reactEl, el as any);
+        // console.log(`${$id}: rendering react uiview into container`, el);
+        if (destroyed) {
+          // console.error(`${$id}: react-ui-view-adapter has already been destroyed -- not rendering React UIView`);
+          return;
+        }
+
+        const props = { ...attrs, render, wrap: false, refFn: ref };
+        // console.log(`${$id}: rendering ReactUIView with props`, props);
+        ReactDOM.render<any>(<ReactUIView {...props}/>, el as any);
       }
 
       scope.$on('$destroy', () => {
-        ReactDOM.unmountComponentAtNode(el);
+        destroyed = true;
+        const unmounted = ReactDOM.unmountComponentAtNode(el);
+        // console.log(`${$id}: angular $destroy event -- unmountComponentAtNode(): ${unmounted}`, el);
         el.remove();
       });
 
@@ -74,3 +96,9 @@ hybridModule.directive('reactUiViewAdapter', function () {
     }
   };
 });
+
+const ReactUIView = ({ refFn, ...props }) => (
+    <UIRouterContextComponent parentContextLevel="3">
+      <UIView {...props} ref={refFn}/>
+    </UIRouterContextComponent>
+);
