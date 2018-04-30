15697c062f353b0bf39f5cb4b358c80a9b1cb381
Chris Thielen <cthielen@netflix.com>
2018-04-30 12:29:22 -0700

    test(cypress): use baseUrl for cypress e2e tests

--- example/cypress/integration/example_spec.js
+++ example/cypress/integration/example_spec.js
@@ -1,44 +1,48 @@
 describe('example app', () => {
   it('loads', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('');
   });
 
   it('renders links', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit('/');
     cy.get('a').contains('react.angular');
     cy.get('a').contains('angular.react');
-    cy.contains('a[ui-sref=angular]', 'angular')
+    cy.contains('a[ui-sref=angular]', 'angular');
   });
 
   it('renders angularjs', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('');
     cy.contains('a[ui-sref=angular]', 'angular').click();
     cy.url().should('include', '#!/angular');
     cy.contains('Hello from angularjs');
   });
 
   it('renders react', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('');
     cy.contains('a[ui-sref=react]', 'react').click();
     cy.url().should('include', '#!/react');
     cy.contains('Hello from react');
   });
 
   it('renders react inside angularjs', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('angular.react').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('angular.react')
+      .click();
     cy.url().should('include', '#!/angular/react');
     cy.contains('Hello from angularjs');
     cy.contains('Hello from react');
   });
 
-
   it('renders angularjs inside react', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('react.angular').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('react.angular')
+      .click();
     cy.url().should('include', '#!/react/angular');
     cy.contains('Hello from react');
     cy.contains('Hello from angularjs');
   });
-
 });
