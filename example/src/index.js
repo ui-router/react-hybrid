b58f252ead4dad33027aafd5f63833f474b05a4b
Chris Thielen <cthielen@netflix.com>
2018-11-12 15:50:40 -0800

    chore(example): Added more nested states to example

--- example/src/index.js
+++ example/src/index.js
@@ -7,17 +7,24 @@ import './style.css';
 const states = [
   { name: 'react', url: '/react', component: ReactComponent },
   { name: 'react.angular', url: '/angular', component: 'angularComponent' },
+  { name: 'react.angular.react', url: '/react', component: ReactComponent },
+  { name: 'react.angular.react.angular', url: '/angular', component: 'angularComponent' },
 
   { name: 'angular', url: '/angular', component: 'angularComponent' },
   { name: 'angular.react', url: '/react', component: ReactComponent },
+  { name: 'angular.react.angular', url: '/angular', component: 'angularComponent' },
+  { name: 'angular.react.angular.react', url: '/react', component: ReactComponent },
 ];
 
 const ngmod = angular.module('app', [UI_ROUTER_REACT_HYBRID]);
-ngmod.config(['$uiRouterProvider', $uiRouterProvider => {
-  states.forEach(state => $uiRouterProvider.stateRegistry.register(state));
-  $uiRouterProvider.urlService.rules.initial({ state: 'home' });
-  $uiRouterProvider.plugin(Visualizer);
-}]);
+ngmod.config([
+  '$uiRouterProvider',
+  $uiRouterProvider => {
+    states.forEach(state => $uiRouterProvider.stateRegistry.register(state));
+    $uiRouterProvider.urlService.rules.initial({ state: 'home' });
+    $uiRouterProvider.plugin(Visualizer);
+  },
+]);
 
 ngmod.component('angularComponent', {
   template: `
@@ -26,7 +33,7 @@ ngmod.component('angularComponent', {
     <ui-view></ui-view>
   `,
   bindings: { $state$: '<' },
-})
+});
 
 const root = document.getElementById('root');
 angular.bootstrap(root, ['app']);
