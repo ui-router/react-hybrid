63bb051d2da8c8fe8c98fd639b9a12fe2efe4c91
Chris Thielen <cthielen@netflix.com>
2018-11-12 16:02:57 -0800

    test(example): Added deeper nesting tests

--- example/cypress/integration/example_spec.js
+++ example/cypress/integration/example_spec.js
@@ -12,24 +12,21 @@ describe('example app', () => {
 
   it('renders angularjs', () => {
     cy.visit('');
-    cy.contains('a[ui-sref=angular]', 'angular').click();
+    cy.get('#angular').click();
     cy.url().should('include', '#!/angular');
     cy.contains('Hello from angularjs');
   });
 
   it('renders react', () => {
     cy.visit('');
-    cy.contains('a[ui-sref=react]', 'react').click();
+    cy.get('#react').click();
     cy.url().should('include', '#!/react');
     cy.contains('Hello from react');
   });
 
   it('renders react inside angularjs', () => {
     cy.visit('');
-    cy
-      .get('a')
-      .contains('angular.react')
-      .click();
+    cy.get('#angular_react').click();
     cy.url().should('include', '#!/angular/react');
     cy.contains('Hello from angularjs');
     cy.contains('Hello from react');
@@ -37,12 +34,33 @@ describe('example app', () => {
 
   it('renders angularjs inside react', () => {
     cy.visit('');
-    cy
-      .get('a')
-      .contains('react.angular')
-      .click();
+    cy.get('#react_angular').click();
     cy.url().should('include', '#!/react/angular');
     cy.contains('Hello from react');
     cy.contains('Hello from angularjs');
+  });
+
+  it('renders angularjs inside react inside angularjs', () => {
+    cy.visit('');
+    cy.get('#angular_react_angular').click();
+    cy.url().should('include', '#!/angular/react/angular');
+  });
+
+  it('renders react inside angularjs inside react', () => {
+    cy.visit('');
+    cy.get('#react_angular_react').click();
+    cy.url().should('include', '#!/react/angular/react');
+  });
+
+  it('renders react inside angularjs inside react inside angularjs', () => {
+    cy.visit('');
+    cy.get('#angular_react_angular_react').click();
+    cy.url().should('include', '#!/angular/react/angular/react');
+  });
+
+  it('renders angularjs inside react inside angularjs inside react', () => {
+    cy.visit('');
+    cy.get('#react_angular_react_angular').click();
+    cy.url().should('include', '#!/react/angular/react/angular');
   });
 });
