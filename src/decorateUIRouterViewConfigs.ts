137dd06603edeaa7cf74929c2ba2711f584d61af
Justas Brazauskas <brazauskasjustas@gmail.com>
2018-12-11 13:36:01 -0500

    Add Suport for componentProvider

--- src/decorateUIRouterViewConfigs.ts
+++ src/decorateUIRouterViewConfigs.ts
@@ -31,9 +31,37 @@ hybridModule.config([
 
       Object.keys(views).forEach(key => {
         const view = views[key];
-        const selfView = selfViews[key || '$default'];
-        const reactType = isReactComponent(view.component) && 'react';
-        view.$type = selfViews[key].$type || reactType || view.$type;
+        if (view.componentProvider) {
+          // If view component provider is given, monkey-patch the type in runtime.
+          const injectables = [...view.componentProvider];
+          // Remove the runner function, and just leave `injectables` with deps.
+          const func = injectables.pop();
+
+          // Create a new component provider.
+          view.componentProvider = [
+            // Original injectables.
+            ...injectables,
+            (...args) => {
+              // Get the component declaration.
+              const cmp = func(...args);
+              // Determine if it's react.
+              var reactType = isReactComponent(cmp) && 'react';
+              view.$type = selfViews[key].$type || reactType || view.$type;
+
+              if (view.$type === 'react') {
+                // Set the component, if it's react, as `ReactViewConfig` expects the `component`
+                // property.
+                view.component = cmp;
+              }
+              // Also return component for AngularjS components.
+              return cmp;
+            },
+          ];
+        } else {
+          var selfView = selfViews[key || '$default'];
+          var reactType = isReactComponent(view.component) && 'react';
+          view.$type = selfViews[key].$type || reactType || view.$type;
+        }
       });
 
       return views;
